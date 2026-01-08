4b5c2b667ab1debcf636ec8b52a13d7c403856dc
AhmedElabrass25 <ahmedelabrass2035@gmail.com>
2026-01-08 22:26:34 +0200

    add stripe payment (ui & logic)

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -54,9 +54,9 @@ export default function AppRoutes() {
               </div>
             }
           />
-          <Route path="payment" element={<PaymentPage />} />
+          <Route path="payment/:id" element={<PaymentPage />} />
           <Route path="payment/success" element={<SuccessPage />} />
-          <Route path="payment/error" element={<ErrorPage />} />
+          <Route path="payment/error/:paymentId" element={<ErrorPage />} />
         </Route>
         <Route path="/welcome" element={<WelcomePage />} />
         <Route path="/login" element={<LoginPage />} />
